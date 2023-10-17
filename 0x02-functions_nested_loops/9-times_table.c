6a9163db3deb66395ba917f86b024eea478eedea
joek-yo <jkihiu129@gmail.com>
2023-10-16 19:25:31 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,35 +3,34 @@
 /**
  * times_table - prints the 9 times table starting with 0
  */
-
 void times_table(void)
 {
 	int i, j, k;
-	for (i = 0; i < 10; i++)
+
+	for (i = 0; i <= 9; i++)
 	{
-		for(j = 0, j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			k = j * i;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
-			if (k < 10 && j != 0)
+			k = i * j;
+			if (j > 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
+
+				if (k < 10)
+					_putchar(' ');
 			}
-			else if (k >= 10)
+			if (k >= 10)
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(k + '0');
 			}
 		}
 		_putchar('\n');
 	}
 }
-
